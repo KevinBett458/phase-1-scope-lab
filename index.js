9b481ac3de7e01e98762f480107d5db99d96660d
KevinBett <kevinbett458@gmail.com>
2023-03-29 19:06:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Declare the customerName variable in global scope and set it to 'bob'
-window.customerName = 'bob';
+customerName = 'bob';
 
 // Define a function to uppercase the customerName variable
 function upperCaseCustomerName() {
-  window.customerName = window.customerName.toUpperCase();
+  customerName = customerName.toUpperCase();
 }
 
 // Define a function to set the bestCustomer variable to 'not bob'
 function setBestCustomer() {
-  window.bestCustomer = 'not bob';
+  bestCustomer = 'not bob';
 }
 
 // Define a function to overwrite the bestCustomer variable with a new value
 function overwriteBestCustomer(newBest) {
-  window.bestCustomer = newBest;
+  bestCustomer = newBest;
 }
 
 // Define a constant variable for the least favorite customer
